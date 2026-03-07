95a35281a74e5165614a9d2dc071c50eb762e9af
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-03-07 17:03:04 +0600

    feat: setup project configuration and dependencies

---
+++ src/main/java/electro_wallet/ElectroWalletApplication.java
@@ -0,0 +1,12 @@
+package electro_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroWalletApplication.class, args);
+    }
+}

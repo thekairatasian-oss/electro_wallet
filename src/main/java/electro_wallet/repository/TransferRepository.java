6983099aa10e2d616bab464d468585493209b9c1
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-03-08 13:20:54 +0600

    feat: added UserRepository, AccountRepository, TransferRepository

---
+++ src/main/java/electro_wallet/repository/TransferRepository.java
@@ -0,0 +1,18 @@
+package electro_wallet.repository;
+
+import electro_wallet.entity.Transfer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+
+        Page<Transfer> findBySenderIdOrReceiverId(Long senderId, Long receiverId, Pageable pageable);
+
+        Page<Transfer> find_All(Pageable pageable);
+
+        Optional<Transfer> find_ById(Long id);
+}
---
+++ src/main/java/electro_wallet/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package electro_wallet.repository;
+
+import electro_wallet.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    boolean existsByEmail(String email);
+}

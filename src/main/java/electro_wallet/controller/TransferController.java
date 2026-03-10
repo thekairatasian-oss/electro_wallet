6569ce5fed241ec4ab0d96b866ae61cf9adea20e
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-03-10 16:02:07 +0600

    feat: добавил новый эндпоит /history для просмотра всех транзакций в TransferController

--- src/main/java/electro_wallet/controller/TransferController.java
+++ src/main/java/electro_wallet/controller/TransferController.java
@@ -5,6 +5,10 @@ import electro_wallet.DTO.TransferResponse;
 import electro_wallet.service.TransferService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +26,11 @@ public class TransferController {
         public ResponseEntity<TransferResponse> transfer(@Valid @RequestBody TransferRequest request, Principal principal)
         {
             return new ResponseEntity<>(transferService.transfer(principal.getName(), request), HttpStatus.CREATED);
+        }
+
+        @GetMapping("/history")
+        public ResponseEntity<Page<TransferResponse>> getAllTransfers(@PageableDefault(size = 10, sort = "timestamp",
+        direction = Sort.Direction.DESC) Pageable pageable, Principal principal) {
+                return ResponseEntity.ok(transferService.getTransactionHistory(principal.getName(), pageable));
         }
 }

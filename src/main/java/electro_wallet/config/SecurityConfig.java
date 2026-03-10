b26bf2edf4e98b72409c17ddfb6eaa4d38b4ddf1
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-03-10 08:58:03 +0600

    feat: added SecurityConfig and passwordEncoder methods and securityfilterchain methods

---
+++ src/main/java/electro_wallet/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package electro_wallet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                        .csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/users/create").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                .httpBasic(Customizer.withDefaults());
+
+                return http.build();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+}

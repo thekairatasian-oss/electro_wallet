bfa449a27b46bef73f9434d6248a9f215bd8d3d8
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-03-12 17:06:35 +0600

    feat: добавил сервис тесты!

---
+++ src/test/java/electro_wallet/ElectroWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package electro_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6589cc248e44d18af8fe835fa2d120cf4dd5fb17
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-04-13 21:39:31 +0600

    Создал Репозитории

---
+++ src/main/java/electro_wallet/repository/TransactionRepository.java
@@ -0,0 +1,25 @@
+package electro_wallet.repository;
+
+import electro_wallet.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository <Transaction, Long> {
+
+    @Query("SELECT t FROM Transaction t WHERE t.sender.accountNumber = :phone OR" +
+            " t.receiver.accountNumber = :phone ORDER BY t.timestamp DESC")
+    List<Transaction> findAllByPhone(@Param("phone") String phone);
+
+    @Query("SELECT t FROM Transaction t WHERE t.sender.accountNumber = :phone ORDER BY t.timestamp DESC")
+    List<Transaction> findOutcoming(@Param("phone") String phone);
+
+    @Query("SELECT t FROM Transaction t WHERE t.receiver.accountNumber = :phone ORDER BY t.timestamp DESC")
+    List<Transaction> findIncoming(@Param("phone") String phone);
+
+}
+
+

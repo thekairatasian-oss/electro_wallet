b1339fbb6f0c8f10b0d45593be76d5332ad21b9f
Kairat Kubanychbekov <thekairatasian@gmail.com>
2026-05-08 18:46:20 +0600

    Начинаю новый учебный проект по переводу денег через номер телефона!

---
+++ src/main/java/electro_wallet/ElectroWalletApplication.java
@@ -0,0 +1,13 @@
+package electro_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroWalletApplication.class, args);
+    }
+
+}
